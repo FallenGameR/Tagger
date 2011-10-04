aa96e888132293aa40c30f6c0cd50abf666bc60f
FallenGameR <Alex.Kostikov@gmail.com>
2011-10-04 01:23:52 -0700

    Moved injection logic to hook project.

--- src/Hook/Hook.h
+++ src/Hook/Hook.h
@@ -9,7 +9,17 @@
 #endif
 
 
-HOOKDLL_API int InjectDll_HookMessageQueue(HWND hWnd, LPSTR lpString);
+HOOKDLL_API void AddDebugPrivilege();
+HOOKDLL_API int InjectDll_HookMessageQueue( HWND hWnd, LPSTR lpString );
+HOOKDLL_API void InjectDll_CreateRemoteThread( DWORD processId, LPCSTR dllPath );
 
+struct error
+{
+    char* Message;
+    DWORD ErrorCode;
+
+    error( char* message ): Message(message), ErrorCode(GetLastError()) {}
+    error( char* message, DWORD lastError): Message(message), ErrorCode(lastError) {}
+};
 
 #endif // !defined(HOOKSPY_DLL_H)
--- src/Injector/Injector.cpp
+++ src/Injector/Injector.cpp
@@ -7,18 +7,6 @@
 
 using namespace std;
 
-void AddDebugPrivilege();
-void InjectDll_CreateRemoteThread( DWORD dwProcessId, LPCSTR lpszDLLPath );
-
-struct error
-{
-    char* Message;
-    DWORD ErrorCode;
-
-    error( char* message ): Message(message), ErrorCode(GetLastError()) {}
-    error( char* message, DWORD lastError): Message(message), ErrorCode(lastError) {}
-};
-
 int main( int argc, char* argv[] )
 {
     if( 1 == argc )
@@ -91,73 +79,3 @@ int main( int argc, char* argv[] )
     return 0;
 }
 
-void AddDebugPrivilege()
-{ 
-    HANDLE token;
-    HANDLE injector = GetCurrentProcess();
-    TOKEN_PRIVILEGES privileges;
-
-    if( 0 == OpenProcessToken( injector, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token ) ) { throw error("OpenProcessToken"); }
-    if( 0 == LookupPrivilegeValue( NULL, SE_DEBUG_NAME, &privileges.Privileges[0].Luid ) ) { throw error("LookupPrivilegeValue"); }
-
-    privileges.PrivilegeCount = 1;
-    privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
-
-    if( 0 == AdjustTokenPrivileges( token, 0, &privileges, sizeof(privileges), NULL, NULL ) ) { throw error("AdjustTokenPrivileges"); }
-}   
-
-void InjectDll_CreateRemoteThread( DWORD processId, LPCSTR dllPath )
-{
-    // TODO: This hook doesn't unload injected DLL itself. But it should.
-    // TODO: Check what OpenProcess flags are really required.
-
-    HANDLE hProcess = NULL;
-    LPVOID lpBaseAddr = NULL;
-    HMODULE hKernel32Dll = NULL;
-    HANDLE hThread = NULL;
-
-    try
-    {
-        hProcess = OpenProcess( PROCESS_CREATE_THREAD|PROCESS_QUERY_INFORMATION|PROCESS_VM_OPERATION|PROCESS_VM_WRITE|PROCESS_VM_READ, FALSE, processId );
-        if( NULL == hProcess ) { throw error("OpenProcess"); }
-
-        DWORD dwMemSize = lstrlenA(dllPath) + 1;
-        lpBaseAddr = VirtualAllocEx( hProcess, NULL, dwMemSize, MEM_COMMIT, PAGE_READWRITE );
-        if( NULL == lpBaseAddr ) { throw error("VirtualAllocEx"); }
-
-        BOOL memoryWriteSuccess = WriteProcessMemory( hProcess, lpBaseAddr, dllPath, dwMemSize, NULL );
-        if( 0 == memoryWriteSuccess ) { throw error("WriteProcessMemory"); }
-
-        hKernel32Dll = LoadLibrary( TEXT("kernel32.dll") );
-        if( NULL == hKernel32Dll ) { throw error("LoadLibrary"); }
-
-        LPVOID lpFuncAddr = GetProcAddress( hKernel32Dll, "LoadLibraryA" );
-        if( NULL == lpFuncAddr ) { throw error("GetProcAddress"); }
-
-        hThread = CreateRemoteThread( hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpFuncAddr, lpBaseAddr, 0, NULL );
-        if( NULL == hThread ) { throw error("CreateRemoteThread"); }
-
-        DWORD waitResult = WaitForSingleObject( hThread, INFINITE );
-        if( WAIT_FAILED == waitResult ) { throw error("WaitForSingleObject"); }
-        if( WAIT_OBJECT_0 != waitResult ) { throw error("WaitForSingleObject returned", waitResult ); }
-
-        DWORD dwExitCode;
-        BOOL exitThreadResult = GetExitCodeThread( hThread, &dwExitCode );
-        if( 0 == exitThreadResult ) { throw error("GetExitCodeThread"); }
-        if( 0 == dwExitCode ) { throw error("Remote LoadLibrary returned", GetLastError()); }
-    }
-    catch( error & )
-    {
-        if( NULL != hThread )  { CloseHandle( hThread ); }
-        if( NULL != hKernel32Dll ) { FreeLibrary( hKernel32Dll ); }
-        if( NULL != lpBaseAddr ) { VirtualFreeEx( hProcess, lpBaseAddr, 0, MEM_RELEASE ); }
-        if( NULL != hProcess ) { CloseHandle( hProcess ); }
-
-        throw;
-    }
-
-    if( NULL != hThread )  { CloseHandle( hThread ); }
-    if( NULL != hKernel32Dll ) { FreeLibrary( hKernel32Dll ); }
-    if( NULL != lpBaseAddr ) { VirtualFreeEx( hProcess, lpBaseAddr, 0, MEM_RELEASE ); }
-    if( NULL != hProcess ) { CloseHandle( hProcess ); }
-}
