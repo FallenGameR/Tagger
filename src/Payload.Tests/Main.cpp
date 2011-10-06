76243a9b049ded07afa6eca5796c85ce9d0ee678
FallenGameR <Alex.Kostikov@gmail.com>
2011-10-06 01:54:05 -0700

    Update for TODO comments.

--- src/Payload.Tests/Main.cpp
+++ src/Payload.Tests/Main.cpp
@@ -2,14 +2,10 @@
 
 int main( int argc, char* argv[] )
 {
-    // TODO: Use unit test library to test for injection
-    // TODO: Remove ps1 tests if not needed
-    // TODO: Add precompiled header to tests
     // TODO: Write test that covers console scenario
     // TODO: Use separate library for exceptions
-    // TODO: /W warning level for all projects.
     // TODO: /Analyse works without any warnings. On Library code. Tests are fine unanalysed.
-    // TODO: Use incremental build. See default project settings.
+    // TODO: Fix all warnings.
 
     return UnitTest::RunAllTests();
 }
