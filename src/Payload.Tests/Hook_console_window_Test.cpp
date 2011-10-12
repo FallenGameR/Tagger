cdde49856c8a8681a48322310da57d6b66582e46
FallenGameR <Alex.Kostikov@gmail.com>
2011-10-12 01:06:58 -0700

    Added links that clarify that console window is really hosted in ConHost.exe or CSRSS.exe

--- src/Payload.Tests/Hook_console_window_Test.cpp
+++ src/Payload.Tests/Hook_console_window_Test.cpp
@@ -8,6 +8,11 @@ TEST( Hook_console_window )
 
     //throw ProgramException("test", 9);
 
+    // http://stackoverflow.com/questions/4706590/changing-window-procedure-for-console-window
+    // http://blogs.msdn.com/b/oldnewthing/archive/2007/12/31/6909007.aspx
+    // http://blogs.technet.com/b/askperf/archive/2009/10/05/windows-7-windows-server-2008-r2-console-host.aspx
+    // 
+
     char buffer[128];
     InjectDll_HookMessageQueue( handle, buffer );
 }
