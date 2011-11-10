491418169080b124d5f83f0642a017dbd24c5698
FallenGameR <Alex.Kostikov@gmail.com>
2011-11-10 00:37:58 -0800

    Simplified PrintWaitChain function.

--- src/Payload.Library/UsingWct.cpp
+++ src/Payload.Library/UsingWct.cpp
@@ -29,12 +29,6 @@ STR_ARRAY STR_OBJECT_TYPE[] =
 // Global variable to store the WCT session handle
 HWCT g_WctHandle = NULL;
 
-//
-// Function prototypes
-//
-
-void PrintWaitChain ( __in DWORD ThreadId);
-
 BOOL GrantDebugPrivilege ( )
 /*++
 
@@ -92,9 +86,39 @@ Return Value:
     return fSuccess;
 }
 
+void PrintWaitChain ( __in DWORD tid )
+{
+    WAITCHAIN_NODE_INFO nodeInfoArray[WCT_MAX_NODE_COUNT];
+    DWORD count, i;
+    BOOL isCycle;
 
+    printf("%d: ", tid);
 
+    count = WCT_MAX_NODE_COUNT;
 
+    // Make a synchronous WCT call to retrieve the wait chain.
+    BOOL success = GetThreadWaitChain(g_WctHandle, NULL, WCTP_GETINFO_ALL_FLAGS, tid, &count, nodeInfoArray, &isCycle);
+    if( !success ) { throw WinApiException("GetThreadWaitChain"); }
+
+    // Check if the wait chain is too big for the array we passed in.
+    if (count > WCT_MAX_NODE_COUNT)
+    {
+        printf("Found additional nodes %d\n", count);
+        count = WCT_MAX_NODE_COUNT;
+    }
+
+    // Loop over all the nodes returned and print useful information.
+    for (i = 0; i < count; i++)
+    {
+        if( WctThreadType == nodeInfoArray[i].ObjectType )
+        {
+            // A thread node contains process and thread ID.
+            printf("[pid %d] | ", nodeInfoArray[i].ThreadObject.ProcessId);
+        }
+    }
+
+    printf("\n");
+}
 
 void CheckThreads( __in DWORD pid )
 {
@@ -135,93 +159,6 @@ void CheckThreads( __in DWORD pid )
 
     CloseHandle(snapshot);
     CloseHandle(process);
-}
-
-void PrintWaitChain ( __in DWORD ThreadId)
-/*++
-
-Routine Description:
-
-    Enumerates all threads (or optionally only threads for one
-    process) in the system. It the calls the WCT API on each thread.
-
-Arguments:
-
-    ThreadId--Specifies the thread ID to analyze.
-
-Return Value:
-
-    (none)
-
---*/
-{
-    WAITCHAIN_NODE_INFO NodeInfoArray[WCT_MAX_NODE_COUNT];
-    DWORD               Count, i;
-    BOOL                IsCycle;
-
-    printf("%d: ", ThreadId);
-
-    Count = WCT_MAX_NODE_COUNT;
-
-    // Make a synchronous WCT call to retrieve the wait chain.
-    if (!GetThreadWaitChain(g_WctHandle, NULL, WCTP_GETINFO_ALL_FLAGS, ThreadId, &Count, NodeInfoArray, &IsCycle))
-    {
-        printf("Error (0X%x)\n", GetLastError());
-        return;
-    }
-
-    // Check if the wait chain is too big for the array we passed in.
-    if (Count > WCT_MAX_NODE_COUNT)
-    {
-        printf("Found additional nodes %d\n", Count);
-        Count = WCT_MAX_NODE_COUNT;
-    }
-
-    // Loop over all the nodes returned and print useful information.
-    for (i = 0; i < Count; i++)
-    {
-        switch (NodeInfoArray[i].ObjectType)
-        {
-            case WctThreadType:
-                // A thread node contains process and thread ID.
-                printf("[pid %d tid %d status %s]->",
-                       NodeInfoArray[i].ThreadObject.ProcessId,
-                       NodeInfoArray[i].ThreadObject.ThreadId,
-                       ((NodeInfoArray[i].ObjectStatus == WctStatusBlocked) ? "b" : "r"));
-                break;
-
-            default:
-                // A synchronization object.
-
-                // Some objects have names...
-                if (NodeInfoArray[i].LockObject.ObjectName[0] != L'\0')
-                {
-                    printf("[description %s name %S]->",
-                           STR_OBJECT_TYPE[NodeInfoArray[i].ObjectType-1].Desc,
-                           NodeInfoArray[i].LockObject.ObjectName);
-                }
-                else
-                {
-                    printf("[description %s]->",
-                           STR_OBJECT_TYPE[NodeInfoArray[i].ObjectType-1].Desc);
-                }
-                if (NodeInfoArray[i].ObjectStatus == WctStatusAbandoned)
-                {
-                    printf("<abandoned>");
-                }
-                break;
-        }
-    }
-
-    printf("[End]");
-
-    // Did we find a deadlock?
-    if (IsCycle)
-    {
-        printf(" !!!Deadlock!!!");
-    }
-
-    printf("\n");
 }
 
 ///
