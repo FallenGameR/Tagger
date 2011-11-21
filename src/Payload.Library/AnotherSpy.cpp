0cdbbf7f10ff196e82b6b67d56d8c28440e9603a
FallenGameR <Alex.Kostikov@gmail.com>
2011-11-21 01:56:31 -0800

    Hooks filters out all noise. Only move, resize and maximize/minimize events are handled.

--- src/Payload.Library/AnotherSpy.cpp
+++ src/Payload.Library/AnotherSpy.cpp
@@ -1,64 +1,63 @@
 #include "stdafx.h"
 #include "DebugConsole.h"
 #include "Payload.Dll.h"
-
-//#if 0
-// Constants
-TCHAR szTitle[] = TEXT("Windows Target x86");
-TCHAR szWindowClass[] = TEXT("TARGET");
-
-// Global Variables
-HWINEVENTHOOK hWinEventHook;
-
-// Forwards
-void Initialization( DWORD processId );
-void Cleanup();
-void CreateTargetConsoleWindow();
-void CALLBACK WinEventProc( HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime );
-LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
-
-HOOKDLL_API void APIENTRY StartHooking( DWORD pid )
-{
-    WNDCLASSEX wcex; 
-    DebugConsole debugConsole;
-
-    // Register window class
-    ZeroMemory( &wcex, sizeof(WNDCLASSEX) );
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.lpfnWndProc = DefWindowProc;
-    wcex.lpszClassName = szWindowClass;
-    ATOM atom = RegisterClassEx( &wcex );
-    if( 0 == atom ) { throw "RegisterClassEx"; }
-
-    // Initialize message window
-    HINSTANCE hInstance = GetModuleHandle(NULL);
-    HWND messageWindow = CreateWindowEx( 0, szWindowClass, szTitle, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL );
-    if( !messageWindow ) { return; }
-
-    hWinEventHook = SetWinEventHook( 
-        EVENT_OBJECT_LOCATIONCHANGE, 
-        EVENT_OBJECT_LOCATIONCHANGE, 
-        NULL, 
-        WinEventProc,
-        pid, 
-        0,
-        WINEVENT_OUTOFCONTEXT ); 
-    if( !hWinEventHook ) { throw "SetWinEventHook"; }
-
-    // Main message loop
-    MSG msg;
-    while( GetMessage( &msg, NULL, 0, 0 ) ) { DispatchMessage( &msg ); }
-
-    UnhookWinEvent( hWinEventHook );
-}
+#include "WinApiException.h"
 
 void CALLBACK WinEventProc( HWINEVENTHOOK hWinEventHook, DWORD eventType, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime )
 {
+    if( OBJID_WINDOW != idObject ) { return; }
+
     RECT rect;
     DWORD dwProcessId;
-    GetWindowThreadProcessId( hwnd, &dwProcessId );
+    DWORD id = GetWindowThreadProcessId( hwnd, &dwProcessId );
 
-    GetWindowRect( hwnd, &rect );
-    cout << eventType <<  ", left: " << rect.left << ", top: " << rect.top << endl;
+    BOOL success = GetWindowRect( hwnd, &rect );
+    cout << 
+        "id: " << id << ", " <<
+        "success: " << success << ", " <<
+        "left: " << rect.left << ", " <<
+        "top: " << rect.top << " ||| " <<
+        "event: " << hWinEventHook << ", " <<
+        "type: " << eventType << ", " <<
+        "hwnd: " << hwnd << ", " <<
+        "object: " << idObject << ", " <<
+        "child: " << idChild << ", " <<
+        "thread: " << dwEventThread << ", " <<
+        "time: " << dwmsEventTime <<               
+        endl;
 }
+
+HOOKDLL_API void APIENTRY StartHooking( DWORD pid )
+{
+    DebugConsole debugConsole;
+
+    // Register window class
+    WNDCLASSEX wcex; 
+    ZeroMemory( &wcex, sizeof(wcex) );
+    wcex.cbSize = sizeof(wcex);
+    wcex.lpfnWndProc = DefWindowProc;
+    wcex.lpszClassName = TEXT("Tagger_hook_class");
+    ATOM atom = RegisterClassEx( &wcex );
+    if( 0 == atom ) { throw  WinApiException("RegisterClassEx"); }
+
+    // Create message window
+    HINSTANCE instance = GetModuleHandle(NULL);
+    if( NULL == instance ) { throw WinApiException("GetModuleHandle"); }
+
+    HWND messageWindow = CreateWindowEx( 0, wcex.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, instance, NULL );
+    if( NULL == messageWindow ) { throw WinApiException("CreateWindowEx"); }
+
+    // Hook selected process
+    HWINEVENTHOOK hook = SetWinEventHook( EVENT_OBJECT_LOCATIONCHANGE, EVENT_OBJECT_LOCATIONCHANGE, NULL, WinEventProc, pid, 0, WINEVENT_OUTOFCONTEXT ); 
+    if( 0 == hook ) { throw WinApiException("SetWinEventHook"); }
+
+    // Main message loop to process hooked events
+    MSG msg;
+    while( GetMessage( &msg, NULL, 0, 0 ) ) { DispatchMessage( &msg ); }
+
+    // Unhook when done
+    UnhookWinEvent( hook );
+}
+
+
 
