e36770a976e06fe671bba42eb240b9ca6dd1bdd1
FallenGameR <Alex.Kostikov@gmail.com>
2011-11-20 20:28:39 -0800

    Refactored IsConsoleApp function.

--- src/Payload.Library/ConsoleDeterminer.cpp
+++ src/Payload.Library/ConsoleDeterminer.cpp
@@ -5,14 +5,6 @@
 #include "WinApiException.h"
 #include "Payload.Dll.h"
 
-//void IsConsoleApp(TCHAR* programPath);
-DWORD AbsoluteSeek( HANDLE, DWORD );
-void ReadFileToBuffer( HANDLE, LPVOID, DWORD );
-void WriteBytes( HANDLE, LPVOID, DWORD );
-void CopySection( HANDLE, HANDLE, DWORD );
-
-#define IMAGE_SIZEOF_NT_OPTIONAL_HEADER sizeof(IMAGE_OPTIONAL_HEADER)
-
 void ReadFileToBuffer( HANDLE file, LPVOID buffer, DWORD bufferSize )
 {
     DWORD bytesRead;
@@ -22,71 +14,36 @@ void ReadFileToBuffer( HANDLE file, LPVOID buffer, DWORD bufferSize )
     if( bufferSize != bytesRead ) { throw WinApiException("ReadFile: unexpected number of bytes were read"); }    
 }
 
-HOOKDLL_API void APIENTRY IsConsoleApp( TCHAR* programPath )
+HOOKDLL_API bool APIENTRY IsConsoleApp( TCHAR* programPath )
 {       
+    // Open file needed to be verified
     HANDLE file = CreateFile( programPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
     if( INVALID_HANDLE_VALUE == file ) { throw WinApiException("CreateFile"); }
 
+    // Verify DOS compability signature (MZ)
     IMAGE_DOS_HEADER image_dos_header;
     ReadFileToBuffer( file, &image_dos_header, sizeof(image_dos_header) ); 
     if( IMAGE_DOS_SIGNATURE != image_dos_header.e_magic ) { throw WinApiException("ReadFileToBuffer: file image signature is unknown"); }
 
-    //DWORD more_dos_header[16];
-    //ReadFileToBuffer( file, more_dos_header, sizeof(more_dos_header) );
-
-    // actual COFF header
+    // Move file pointer to actual COFF header
     DWORD coffHeaderOffset = SetFilePointer( file, image_dos_header.e_lfanew, NULL, FILE_BEGIN );
-    if( 0xFFFFFFFF == coffHeaderOffset ) { throw WinApiException("SetFilePointer"); } 
+    if( INVALID_SET_FILE_POINTER == coffHeaderOffset ) { throw WinApiException("SetFilePointer"); } 
 
+    // Verify NT signature correctness (PE00)
     ULONG ntSignature;
     ReadFileToBuffer( file, &ntSignature, sizeof(ntSignature) );
     if( IMAGE_NT_SIGNATURE != ntSignature ) { throw WinApiException("ReadFileToBuffer: missing NT signature"); }
 
+    // Read file header. File pointer is moved forward.
     IMAGE_FILE_HEADER image_file_header;
-    DWORD test = sizeof(IMAGE_FILE_HEADER);
-    ReadFileToBuffer(file, &image_file_header, IMAGE_SIZEOF_FILE_HEADER);
+    ReadFileToBuffer(file, &image_file_header, sizeof(IMAGE_FILE_HEADER));
 
+    // Read file optional header that is located after image file header
     IMAGE_OPTIONAL_HEADER image_optional_header;
-    ReadFileToBuffer(file, &image_optional_header, IMAGE_SIZEOF_NT_OPTIONAL_HEADER);
+    ReadFileToBuffer(file, &image_optional_header, sizeof(IMAGE_OPTIONAL_HEADER));
 
-    switch( image_optional_header.Subsystem )
-    {
-    case IMAGE_SUBSYSTEM_UNKNOWN:
-        printf("Type is unknown.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_NATIVE:
-        printf("Type is native.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_WINDOWS_GUI:
-        printf("Type is Windows GUI.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_WINDOWS_CUI:
-        printf("Type is Windows CUI.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_OS2_CUI:
-        printf("Type is OS/2 CUI.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_POSIX_CUI:
-        printf("Type is POSIX CUI.\n");
-        break;
-
-    case IMAGE_SUBSYSTEM_NATIVE_WINDOWS:
-           printf("Type is native Win9x driver.\n");
-           break;
-
-    case IMAGE_SUBSYSTEM_WINDOWS_CE_GUI:
-        printf("Type is Windows CE.\n");
-        break;
-
-    default:
-        printf("Unknown type %u.\n", image_optional_header.Subsystem);
-        break;
-    }
+    // Return true if file has Console subsystem
+    return IMAGE_SUBSYSTEM_WINDOWS_CUI == image_optional_header.Subsystem;
 }
 
 
--- src/Payload.Tests/Is_console_app_Tests.cpp
+++ src/Payload.Tests/Is_console_app_Tests.cpp
@@ -3,18 +3,21 @@
 
 TEST( Cmd_is_console_app_Test )
 {
-    TCHAR* cmdPath = TEXT("C:\\Windows\\system32\\cmd.exe");
-    IsConsoleApp(cmdPath);
+    TCHAR* path = TEXT("C:\\Windows\\system32\\cmd.exe");
+    bool isCui = IsConsoleApp(path);
+    CHECK( isCui );    
 }
 
 TEST( Powershell_is_console_app_Test )
 {
-    TCHAR* cmdPath = TEXT("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
-    IsConsoleApp(cmdPath);
+    TCHAR* path = TEXT("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
+    bool isCui = IsConsoleApp(path);
+    CHECK( isCui );    
 }
 
 TEST( Notepad_is_not_console_app_Test )
 {
-    TCHAR* cmdPath = TEXT("C:\\Windows\\system32\\notepad.exe");
-    IsConsoleApp(cmdPath);
+    TCHAR* path = TEXT("C:\\Windows\\system32\\notepad.exe");
+    bool isCui = IsConsoleApp(path);
+    CHECK( !isCui );    
 }
