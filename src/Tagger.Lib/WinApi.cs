e263aebaca472aec9101f9076561b0c3e4e206a7
FallenGameR <Alex.Kostikov@gmail.com>
2011-11-26 01:38:51 -0800

    Code for getting snapshot tids for a process (way simpler that in c++ =)

---
+++ src/Tagger.Lib/WinApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Tagger.Lib
+{
+    public static class WinApi
+    {
+        public static void Test( int pid )
+        {
+            var process = Process.GetProcessById( pid );
+            var tids = process.Threads.Cast<ProcessThread>().Select( t => t.Id );
+          
+        }
+    }
+}
