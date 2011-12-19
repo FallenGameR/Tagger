ca9194f1db84e6cd535a2b8b794ba0249ebfc163
FallenGameR <Alex.Kostikov@gmail.com>
2011-12-18 16:36:10 -0800

    Removed unreferenced projects.

--- src/Payload.Tests/Wct_call_Test.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "process.h"
-
-TEST( Wct_call_Test )
-{
-    Process process( TEXT("Host.Console.exe") );
-    int returnCode = FindConhost( process.GetPid() );
-    CHECK( 0 != returnCode );
-}
