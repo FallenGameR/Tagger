e8975add4ecc20c736750fa6f8c79f94b1d3ac39
FallenGameR <Alex.Kostikov@gmail.com>
2011-12-28 00:48:59 -0800

    Discovered GetForegroundWindow API. Most likely will delete half of the project because of it =)

--- src/Tagger.Wpf/Domain/HotkeyHandler.cs
+++ src/Tagger.Wpf/Domain/HotkeyHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Tagger.Wpf
 {
@@ -10,7 +11,29 @@ namespace Tagger.Wpf
     {
         public static void HandleHotkeyPress()
         {
-            MessageBox.Show("Hotkey was pressed");
+            MessageBox.Show("Current window: " + GetActiveWindowTitle());
+
         }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return string.Empty;
+        }
+
     }
 }
---
+++ src/Tagger.Wpf/ViewModels/OverlayViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Utils.Prism;
+
+namespace Tagger.Wpf.ViewModels
+{
+    public class OverlayViewModel: ViewModelBase
+    {
+        public OverlayViewModel(int hostProcessId)
+        {
+            // NOTE: Borders sizes - http://stackoverflow.com/questions/431470/window-border-width-and-height-in-win32-how-do-i-get-it
+        }
+    }
+}
