027fad6dcf0371146f4d7269e46b9a7ef6d5c029
FallenGameR <Alex.Kostikov@gmail.com>
2012-02-22 12:26:43 -0800

    Updated license info.

--- src/Tagger.Wpf/Properties/AssemblyInfo.cs
+++ src/Tagger.Wpf/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Tagger" )]
-[assembly: AssemblyCopyright( "Copyright © FallenGameR 2012" )]
+[assembly: AssemblyCopyright( "Copyright © Alexander Kostikov (FallenGameR) 2012" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
