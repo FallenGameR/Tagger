bafa6dbc33aeee0970193864a5bd9c74019b1faf
Alexander Kostikov <alexko@microsoft.com>
2012-02-24 15:12:47 -0800

    Fixed color selection - now it's possible to specify color by typing.

--- src/Tagger.Wpf/Windows/SettingsWindow.xaml.cs
+++ src/Tagger.Wpf/Windows/SettingsWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System.Windows;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tagger.Wpf.Windows
 {
     public partial class SettingsWindow : Window
     {
+        public static string[] ColorNames = typeof(Colors).GetProperties().Select( pi => pi.Name).ToArray();
+
         public SettingsWindow()
         {
             InitializeComponent();
