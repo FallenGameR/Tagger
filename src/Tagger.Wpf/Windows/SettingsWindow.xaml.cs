45a6106e49b24ec603e30c873499faa36fbf1a7b
FallenGameR <Alex.Kostikov@gmail.com>
2012-02-25 00:23:10 -0800

    Few settings window improvements.

--- src/Tagger.Wpf/Windows/SettingsWindow.xaml.cs
+++ src/Tagger.Wpf/Windows/SettingsWindow.xaml.cs
@@ -1,17 +1,63 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Data;
 using System.Windows.Media;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Tagger.Wpf.Windows
 {
+    /// <summary>
+    /// Settings window that sets up how tag looks like
+    /// </summary>
+    /// <remarks>
+    /// Only view-related code here
+    /// </remarks>
     public partial class SettingsWindow : Window
     {
+        /// <summary>
+        /// Color names resource
+        /// </summary>
+        /// <remarks>
+        /// Is not moved to XAML since in C# it's way more readable to call Select from LINQ
+        /// </remarks>
         public static string[] ColorNames = typeof(Colors).GetProperties().Select( pi => pi.Name).ToArray();
 
+        /// <summary>
+        /// Initializes settings window
+        /// </summary>
         public SettingsWindow()
         {
             InitializeComponent();
+            this.IsVisibleChanged += new DependencyPropertyChangedEventHandler(SettingsWindow_IsVisibleChanged);
+        }
+
+        /// <summary>
+        /// Set focus and select all tag text on settings dialog shown
+        /// </summary>
+        private void SettingsWindow_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                this.SelectTagText();
+            }
+        }
+
+        /// <summary>
+        /// Workaround for late data binding for the settings dialog shown for the first time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextTxt_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            this.SelectTagText();
+        }
+
+        /// <summary>
+        /// Focus and select all text of the tag to make it easily editable
+        /// </summary>
+        private void SelectTagText()
+        {
+            this.TextTxt.Focus();
+            this.TextTxt.SelectAll();
         }
     }
 }
