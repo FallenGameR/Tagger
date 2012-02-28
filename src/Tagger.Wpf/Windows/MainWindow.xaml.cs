5bb4b08821fe60c1925497da13194412d30eac48
FallenGameR <Alex.Kostikov@gmail.com>
2012-02-28 00:44:30 -0800

    Commented out assigning ex_toolwindow style.

--- src/Tagger.Wpf/Windows/MainWindow.xaml.cs
+++ src/Tagger.Wpf/Windows/MainWindow.xaml.cs
@@ -15,20 +15,20 @@ namespace Tagger.Wpf
             // TODO: Check windows OS edition
             InitializeComponent();
 
-            this.WindowStyle = System.Windows.WindowStyle.None;
-            this.ShowInTaskbar = false;
+            //this.WindowStyle = System.Windows.WindowStyle.None;
+            //this.ShowInTaskbar = false;
 
             this.Closed += delegate { App.Current.Shutdown(); };
         }      
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+            //WindowInteropHelper wndHelper = new WindowInteropHelper(this);
 
-            int exStyle = (int)NativeAPI.GetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE);
+            //int exStyle = (int)NativeAPI.GetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE);
 
-            exStyle |= (int)NativeAPI.ExtendedWindowStyles.WS_EX_TOOLWINDOW;
-            NativeAPI.SetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+            //exStyle |= (int)NativeAPI.ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            //NativeAPI.SetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
         }
 
         private void Window_StateChanged(object sender, EventArgs e)
