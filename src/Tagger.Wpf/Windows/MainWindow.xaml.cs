382d5c75f14ed5daf331c7efc457cacc60b77c4f
FallenGameR <Alex.Kostikov@gmail.com>
2012-02-28 23:11:10 -0800

    Removed WS_EX_TOOLWINDOW related code - I need something else.

--- src/Tagger.Wpf/Windows/MainWindow.xaml.cs
+++ src/Tagger.Wpf/Windows/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Interop;
-using Tagger.WinApi;
 using System;
 
 namespace Tagger.Wpf
@@ -15,26 +14,14 @@ namespace Tagger.Wpf
             // TODO: Check windows OS edition
             InitializeComponent();
 
-            //this.WindowStyle = System.Windows.WindowStyle.None;
-            //this.ShowInTaskbar = false;
-
             this.Closed += delegate { App.Current.Shutdown(); };
         }      
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            //WindowInteropHelper wndHelper = new WindowInteropHelper(this);
-
-            //int exStyle = (int)NativeAPI.GetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE);
-
-            //exStyle |= (int)NativeAPI.ExtendedWindowStyles.WS_EX_TOOLWINDOW;
-            //NativeAPI.SetWindowLong(wndHelper.Handle, (int)NativeAPI.GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
-        }
 
         private void Window_StateChanged(object sender, EventArgs e)
         {
             if (this.WindowState == WindowState.Minimized)
             {
+                this.Hide();
                 this.ShowInTaskbar = false;
             }
         }
