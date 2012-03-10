03823144ab486e825dccce4bbe15922dc866345b
FallenGameR <Alex.Kostikov@gmail.com>
2012-03-09 22:52:03 -0800

    Added dependency property 'purpose' to hotkey control.

--- src/Tagger.Wpf/Views/HotkeyControl.xaml.cs
+++ src/Tagger.Wpf/Views/HotkeyControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows;
 
 namespace Tagger.Wpf.Views
 {
@@ -10,6 +11,24 @@ namespace Tagger.Wpf.Views
         public HotkeyControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Dependency propery for Purpose property
+        /// </summary>
+        public static readonly DependencyProperty PurposeProperty = DependencyProperty.Register(
+            "Purpose",
+            typeof(string),
+            typeof(HotkeyControl),
+            new UIPropertyMetadata("Shortcut key"));
+
+        /// <summary>
+        /// Purpose of the hotkey, what it is used for
+        /// </summary>
+        public string Purpose
+        {
+            get { return (string)this.GetValue(PurposeProperty); }
+            set { this.SetValue(PurposeProperty, value); }
         }
     }
 }
