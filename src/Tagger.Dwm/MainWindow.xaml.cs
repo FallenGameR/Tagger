b187e48cb4795520c654fa46fb8c8d35533fb704
FallenGameR <Alex.Kostikov@gmail.com>
2012-03-19 00:49:53 -0700

    Reduced client code to minimum.

--- src/Tagger.Dwm/MainWindow.xaml.cs
+++ src/Tagger.Dwm/MainWindow.xaml.cs
@@ -2,16 +2,10 @@
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 using Tagger.WinAPI;
-using Utils.Extensions;
-using System.Windows.Media;
-using System.Windows.Documents;
-using System.Collections.Generic;
-using Utils.Diagnostics;
 
 namespace Tagger.Dwm
 {
@@ -25,57 +19,48 @@ namespace Tagger.Dwm
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += delegate { this.RebindWindowItems(); };
-        }
 
-        private IntPtr Handle
-        {
-            get { return new WindowInteropHelper(this).Handle; }
-        }
-        
-        /// <summary>
-        /// NOTE: Would not be reused
-        /// </summary>
-        private void RebindWindowItems()
-        {
-            lstWindows.Items.Clear();
-
-            var success = NativeAPI.EnumWindows( (hwnd, lParam) =>
+            // NOTE: Would not be reused
+            this.Loaded += delegate
             {
-                var windowLong = NativeAPI.GetWindowLong(hwnd, NativeAPI.GWL_STYLE);
-                if (windowLong == 0)
+                lstWindows.Items.Clear();
+
+                var success = NativeAPI.EnumWindows((hwnd, lParam) =>
+                {
+                    var windowLong = NativeAPI.GetWindowLong(hwnd, NativeAPI.GWL_STYLE);
+                    if (windowLong == 0)
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+
+                    const ulong TARGETWINDOW = NativeAPI.WS_BORDER | NativeAPI.WS_VISIBLE;
+                    var handle = new WindowInteropHelper(this).Handle;
+
+                    if (handle != hwnd && (windowLong & TARGETWINDOW) == TARGETWINDOW)
+                    {
+                        var sb = new StringBuilder(100);
+                        var ress = NativeAPI.GetWindowText(hwnd, sb, sb.Capacity);
+                        var err = Marshal.GetLastWin32Error();
+                        if ((ress == 0) && (err != NativeAPI.NO_ERROR))
+                        {
+                            throw new Win32Exception(err);
+                        }
+
+                        lstWindows.Items.Add(new WindowItem
+                        {
+                            Handle = hwnd,
+                            Title = sb.ToString(),
+                        });
+                    }
+
+                    return true; //continue enumeration
+                }, 0);
+
+                if (success == 0)
                 {
                     throw new Win32Exception(Marshal.GetLastWin32Error());
                 }
-
-                const ulong TARGETWINDOW = NativeAPI.WS_BORDER | NativeAPI.WS_VISIBLE;
-
-                if (this.Handle != hwnd && (windowLong & TARGETWINDOW) == TARGETWINDOW)
-                {
-                    var sb = new StringBuilder(100);
-                    var ress = NativeAPI.GetWindowText(hwnd, sb, sb.Capacity);
-                    var err = Marshal.GetLastWin32Error();
-                    if ((ress == 0) && ( err != NativeAPI.NO_ERROR))
-                    {
-                        throw new Win32Exception(err);
-                    }
-
-                    lstWindows.Items.Add(new WindowItem
-                    {
-                        Handle = hwnd,
-                        Title = sb.ToString(),
-                    });
-                }
-
-                return true; //continue enumeration
-            }, 0);
-
-            if (success == 0)
-            {
-                throw new Win32Exception(Marshal.GetLastWin32Error());
-            }
-
-                
+            };
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -83,7 +68,6 @@ namespace Tagger.Dwm
             if (this.thumbnail != null)
             {
                 this.thumbnail.Dispose();
-                this.thumbnail = null;
             }
 
             var window = (WindowItem)lstWindows.SelectedItem;
