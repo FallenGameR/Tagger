a1462a5294269430bb7aa7732611bffda81daab5
FallenGameR <Alex.Kostikov@gmail.com>
2012-03-20 23:07:04 -0700

    Added glass effect to groups window.

--- src/Tagger.Wpf/Windows/GroupsWindow.xaml.cs
+++ src/Tagger.Wpf/Windows/GroupsWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace Tagger.Windows
 {
@@ -21,6 +23,55 @@ namespace Tagger.Windows
         public GroupsWindow()
         {
             InitializeComponent();
+            this.Loaded += delegate
+            {
+                GlassHelper.ExtendGlassFrame(this, new Thickness(-1));
+            };
         }
     }
+
+    public class GlassHelper
+    {
+        struct MARGINS
+        {
+            public MARGINS(Thickness t)
+            {
+                Left = (int)t.Left;
+                Right = (int)t.Right;
+                Top = (int)t.Top;
+                Bottom = (int)t.Bottom;
+            }
+            public int Left;
+            public int Right;
+            public int Top;
+            public int Bottom;
+        }
+
+        [DllImport("dwmapi.dll")]
+        static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
+
+        [DllImport("dwmapi.dll")]
+        static extern bool DwmIsCompositionEnabled();
+
+        public static bool ExtendGlassFrame(Window window, Thickness margin)
+        {
+            if (!DwmIsCompositionEnabled())
+                return false;
+
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            if (hwnd == IntPtr.Zero)
+                throw new InvalidOperationException("The Window must be shown before extending glass.");
+
+            window.Background = Brushes.Transparent;
+            HwndSource.FromHwnd(hwnd).CompositionTarget.BackgroundColor = Colors.Transparent;
+
+            var margins = new MARGINS(margin);
+            DwmExtendFrameIntoClientArea(hwnd, ref margins);
+            return true;
+        }
+
+
+
+    }
+
 }
