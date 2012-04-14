4e007064f87fc1e6715efd358e83d91918b45e51
FallenGameR <Alex.Kostikov@gmail.com>
2012-04-14 15:21:26 -0700

    Removed Glass and Thumnail classes. DesktopWindowManager is no longer used anywhere.

--- src/Tagger.Lib/Utils/Extensions/Wpf.cs
+++ src/Tagger.Lib/Utils/Extensions/Wpf.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 
@@ -7,38 +6,6 @@ namespace Utils.Extensions
 {
     public static class Wpf
     {
-        /// <summary>
-        /// Get top left corner location of a UI element
-        /// </summary>
-        /// <param name="element">Element which location is queried</param>
-        /// <returns>Point coordinates of top left corner of the UI element</returns>
-        public static Point GetLocation(this UIElement element)
-        {
-            return element.TranslatePoint(
-                new Point(0, 0),
-                element.GetTopLevelElement());
-        }
-
-        /// <summary>
-        /// Get top level UI element object starting from seed
-        /// </summary>
-        /// <param name="seed">Start point of the lookup</param>
-        /// <returns>The top UI element object (Window or Page)</returns>
-        public static UIElement GetTopLevelElement(this UIElement seed)
-        {
-            var parents = new List<DependencyObject>();
-
-            DependencyObject current = seed;
-            while (true)
-            {
-                current = VisualTreeHelper.GetParent(current);
-                if (current == null) { break; }
-                parents.Add(current);
-            }
-
-            return (UIElement)parents.LastOrDefault(depObj => depObj is UIElement) ?? seed;
-        }
-
         /// <summary>
         /// Get all visual tree children for an element
         /// </summary>
