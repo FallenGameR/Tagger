57485c42666a798017115647d8a34b3697f608e3
FallenGameR <Alex.Kostikov@gmail.com>
2012-04-24 00:01:02 -0700

    stylecop for PortableExecutableReader.cs

--- src/Tagger.Lib/Domain/PortableExecutableReader.cs
+++ src/Tagger.Lib/Domain/PortableExecutableReader.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
-using Tagger.WinAPI;
+﻿//-----------------------------------------------------------------------
+// <copyright file="PortableExecutableReader.cs" company="none">
+//  Distributed under the 3-clause BSD license
+//  Copyright (c) Alexander Kostikov
+//  All rights reserved
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Tagger
 {
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using Tagger.WinAPI;
+
     /// <summary>
     /// Reads Portable Executable header info
     /// </summary>
@@ -13,11 +20,10 @@ namespace Tagger
     /// </remarks>
     public class PortableExecutableReader
     {
-        #region Methods
-
         /// <summary>
-        /// Constructor
+        /// Initializes a new instance of the <see cref="PortableExecutableReader"/> class. 
         /// </summary>
+        /// <param name="filePath">Path to the file to read</param>
         public PortableExecutableReader(string filePath)
         {
             // Read in the DLL or EXE and get the timestamp
@@ -25,72 +31,51 @@ namespace Tagger
             using (var reader = new BinaryReader(stream))
             {
                 // Read DOS header
-                this.DosHeader = PortableExecutableReader.ReadStructure<NativeAPI.IMAGE_DOS_HEADER>(reader);
+                this.DosHeader = ReadStructure<NativeAPI.IMAGE_DOS_HEADER>(reader);
 
                 // Add 4 bytes to the offset
                 stream.Seek(this.DosHeader.e_lfanew, SeekOrigin.Begin);
 
                 // Skip NT header signature 
-                UInt32 ntHeadersSignature = reader.ReadUInt32();
+                reader.ReadUInt32();
 
                 // Read file header
-                this.FileHeader = PortableExecutableReader.ReadStructure<NativeAPI.IMAGE_FILE_HEADER>(reader);
+                this.FileHeader = ReadStructure<NativeAPI.IMAGE_FILE_HEADER>(reader);
 
                 // Read optional header
                 if (this.IsOptionalHeader32)
                 {
-                    this.OptionalHeader32 = PortableExecutableReader.ReadStructure<NativeAPI.IMAGE_OPTIONAL_HEADER32>(reader);
+                    this.OptionalHeader32 = ReadStructure<NativeAPI.IMAGE_OPTIONAL_HEADER32>(reader);
                 }
                 else
                 {
-                    this.OptionalHeader64 = PortableExecutableReader.ReadStructure<NativeAPI.IMAGE_OPTIONAL_HEADER64>(reader);
+                    this.OptionalHeader64 = ReadStructure<NativeAPI.IMAGE_OPTIONAL_HEADER64>(reader);
                 }
             }
         }
 
         /// <summary>
-        /// Reads in a block from a file and converts it to the struct 
-        /// type specified by the template parameter
-        /// </summary>
-        private static T ReadStructure<T>(BinaryReader reader)
-        {
-            // Read in a byte array
-            byte[] bytes = reader.ReadBytes(Marshal.SizeOf(typeof(T)));
-
-            // Pin the managed memory while, copy it out the data, then unpin it
-            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            T theStructure = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
-            handle.Free();
-
-            return theStructure;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// The DOS header
+        /// Gets DOS header
         /// </summary>
         public NativeAPI.IMAGE_DOS_HEADER DosHeader { get; private set; }
 
         /// <summary>
-        /// The file header
+        /// Gets file header
         /// </summary>
         public NativeAPI.IMAGE_FILE_HEADER FileHeader { get; private set; }
 
         /// <summary>
-        /// Optional 32 bit file header
+        /// Gets optional 32 bit file header
         /// </summary>
         public NativeAPI.IMAGE_OPTIONAL_HEADER32 OptionalHeader32 { get; private set; }
 
         /// <summary>
-        /// Optional 64 bit file header
+        /// Gets optional 64 bit file header
         /// </summary>
         public NativeAPI.IMAGE_OPTIONAL_HEADER64 OptionalHeader64 { get; private set; }
 
         /// <summary>
-        /// Gets if the file header is 32 bit or not
+        /// Gets a value indicating whether the file header is 32 bit or not
         /// </summary>
         public bool IsOptionalHeader32
         {
@@ -98,18 +83,36 @@ namespace Tagger
         }
 
         /// <summary>
-        /// Optional header without bitwise distinction
+        /// Gets optional header without bitwise distinction
         /// </summary>
         public dynamic OptionalHeader
         {
             get
             {
                 return this.IsOptionalHeader32
-                    ? (dynamic) this.OptionalHeader32
-                    : (dynamic) this.OptionalHeader64;
+                    ? (dynamic)this.OptionalHeader32
+                    : (dynamic)this.OptionalHeader64;
             }
         }
 
-        #endregion
+        /// <summary>
+        /// Reads in a block from a file and converts it to the struct 
+        /// type specified by the template parameter
+        /// </summary>
+        /// <typeparam name="T">Type of the output structure</typeparam>
+        /// <param name="reader">Reader object that is used to read file</param>
+        /// <returns>Structure read from the current file position</returns>
+        private static T ReadStructure<T>(BinaryReader reader)
+        {
+            // Read in a byte array
+            byte[] bytes = reader.ReadBytes(Marshal.SizeOf(typeof(T)));
+
+            // Pin the managed memory while, copy it out the data, then unpin it
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            var theStructure = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            handle.Free();
+
+            return theStructure;
+        }
     }
 }
