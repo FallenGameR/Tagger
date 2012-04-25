05fec69eb9f8779f05fd3afac7850cb4e61fdced
FallenGameR <Alex.Kostikov@gmail.com>
2012-04-24 23:14:16 -0700

    StyleCop for design by contract and program exception.

--- src/Tagger.Lib/Utils/Diagnostics/DesignByContractException.cs
+++ src/Tagger.Lib/Utils/Diagnostics/DesignByContractException.cs
@@ -1,20 +1,56 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿//-----------------------------------------------------------------------
+// <copyright file="DesignByContractException.cs" company="none">
+//  Distributed under the 3-clause BSD license
+//  Copyright (c) Alexander Kostikov
+//  All rights reserved
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Utils.Diagnostics
 {
+    using System;
+    using System.Runtime.Serialization;
+
     /// <summary>
-    /// Exceptions when code contract is violated
+    /// Exception thrown when code contract is violated
     /// </summary>
-    /// <remarks>
-    /// Taken from NHibernate Best Practices (on CodeProject site)
-    /// </remarks>
     [Serializable]
     public class DesignByContractException : ProgramException
     {
-        internal DesignByContractException() { }
-        internal DesignByContractException(string message) : base(message) { }
-        internal DesignByContractException(string message, Exception inner) : base(message, inner) { }
-        internal DesignByContractException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DesignByContractException"/> class.
+        /// </summary>
+        internal DesignByContractException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DesignByContractException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        internal DesignByContractException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DesignByContractException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="inner">Inner exception</param>
+        internal DesignByContractException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DesignByContractException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Streaming context</param>
+        internal DesignByContractException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
--- src/Tagger.Lib/Utils/Diagnostics/ProgramException.cs
+++ src/Tagger.Lib/Utils/Diagnostics/ProgramException.cs
@@ -1,17 +1,56 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProgramException.cs" company="none">
+//  Distributed under the 3-clause BSD license
+//  Copyright (c) Alexander Kostikov
+//  All rights reserved
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Utils.Diagnostics
 {
+    using System;
+    using System.Runtime.Serialization;
+
     /// <summary>
     /// Root of all program's exceptions
     /// </summary>
     [Serializable]
     public class ProgramException : ApplicationException
     {
-        public ProgramException() { }
-        public ProgramException(string message) : base(message) { }
-        public ProgramException(string message, Exception inner) : base(message, inner) { }
-        protected ProgramException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgramException"/> class.
+        /// </summary>
+        public ProgramException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgramException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public ProgramException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgramException"/> class.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="inner">Inner exception</param>
+        public ProgramException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgramException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Streaming context</param>
+        protected ProgramException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
