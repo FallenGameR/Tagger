359dd200aede9c53d351977196025f6a8a6ea4f0
FallenGameR <Alex.Kostikov@gmail.com>
2012-04-25 21:51:13 -0700

    HotkeyEventArgs cleaned up

--- src/Tagger.Lib/Utils/HotkeyEventArgs.cs
+++ src/Tagger.Lib/Utils/HotkeyEventArgs.cs
@@ -1,31 +1,44 @@
-﻿using System;
-using System.Windows.Forms;
-using System.Windows.Input;
+﻿//-----------------------------------------------------------------------
+// <copyright file="HotkeyEventArgs.cs" company="none">
+//  Distributed under the 3-clause BSD license
+//  Copyright (c) Alexander Kostikov
+//  All rights reserved
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Tagger
 {
+    using System;
+    using System.Windows.Forms;
+    using System.Windows.Input;
+
     /// <summary>
     /// Event arguments for global hotkey press event
     /// </summary>
     public class HotkeyEventArgs : EventArgs
     {
         /// <summary>
-        /// Constructs readable arguments for global hotkey event
+        /// Initializes a new instance of the <see cref="HotkeyEventArgs"/> class. 
         /// </summary>
-        /// <param name="wmHotkeyLParam">LParam value passed as argument to wm_Hotkey message</param>
-        public HotkeyEventArgs(int wmHotkeyLParam)
+        /// <param name="messageHotkeyParam">
+        /// LParam value passed as argument to wm_Hotkey message
+        /// </param>
+        /// <remarks>
+        /// Constructs readable arguments for global hotkey event
+        /// </remarks>
+        public HotkeyEventArgs(int messageHotkeyParam)
         {
-            this.Key = (Keys)((wmHotkeyLParam >> 16) & 0xFFFF);
-            this.Modifier = (ModifierKeys)(wmHotkeyLParam & 0xFFFF);
+            this.Key = (Keys)((messageHotkeyParam >> 16) & 0xFFFF);
+            this.Modifier = (ModifierKeys)(messageHotkeyParam & 0xFFFF);
         }
 
         /// <summary>
-        /// Key pressed
+        /// Gets key that was pressed
         /// </summary>
         public Keys Key { get; private set; }
 
         /// <summary>
-        /// Key modifiers pressed
+        /// Gets key modifiers that were pressed
         /// </summary>
         public ModifierKeys Modifier { get; private set; }
     }
