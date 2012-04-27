3c5a34b298e49bdb90abafd1ea2a2627073b0127
FallenGameR <Alex.Kostikov@gmail.com>
2012-04-26 22:01:08 -0700

    HotkeyViewModel stylecop

--- src/Tagger.Wpf/ViewModels/HotkeyViewModel.cs
+++ src/Tagger.Wpf/ViewModels/HotkeyViewModel.cs
@@ -28,29 +28,14 @@ namespace Tagger.ViewModels
         /// <summary>
         /// Status field
         /// </summary>
-        private string m_Status;
+        private string status;
 
         /// <summary>
-        /// Check if key is a modifier (Ctrl, Alt, Shift)
+        /// Initializes a new instance of the <see cref="HotkeyViewModel"/> class. 
         /// </summary>
-        /// <param name="key">Key to check</param>
-        /// <returns>true if key is a modifier</returns>
-        private static bool IsModifierKey(Key key)
-        {
-            return key == Key.LeftShift
-                || key == Key.RightShift
-                || key == Key.LeftCtrl
-                || key == Key.RightCtrl
-                || key == Key.LeftAlt
-                || key == Key.RightAlt
-                || key == Key.LWin
-                || key == Key.RWin;
-        }
-
-        /// <summary>
-        /// Initializes new instance of HotkeyViewModel class
-        /// </summary>
-        /// <param name="control">Control to be bound to</param>
+        /// <param name="control">
+        /// Control to be bound to
+        /// </param>
         public HotkeyViewModel(HotkeyView control)
         {
             Check.Require(control != null, "Hotkey control should not be null");
@@ -68,48 +53,49 @@ namespace Tagger.ViewModels
         }
 
         /// <summary>
-        /// Unregister previously registered global hotkey
+        /// Gets command that unregisters previously registered global hotkey
         /// </summary>
         public DelegateCommand<object> UnregisterHotkeyCommand { get; private set; }
 
         /// <summary>
-        /// Modifier keys used for hotkey 
+        /// Gets or sets modifier keys used for hotkey 
         /// </summary>
         public ModifierKeys ModifierKeys { get; set; }
 
         /// <summary>
-        /// Key used in hotkey
+        /// Gets or sets key used in hotkey
         /// </summary>
         public Key Key { get; set; }
 
         /// <summary>
-        /// Hotkey registration status
+        /// Gets hotkey registration status
         /// </summary>
         public string Status
         {
             get
             {
-                return this.m_Status;
+                return this.status;
             }
+
             private set
             {
-                this.m_Status = value;
-                OnPropertyChanged(this.Property(() => Status));
+                this.status = value;
+                this.OnPropertyChanged(this.Property(() => this.Status));
             }
         }
 
         /// <summary>
-        /// Global hotkey object that is used to watch for hotkey event
+        /// Gets or sets global hotkey object that is used to watch for hotkey event
         /// </summary>
         private GlobalHotkey GlobalHotkey { get; set; }
 
         /// <summary>
-        /// Handler for the global hotkey
+        /// Gets or sets handler for the global hotkey
         /// </summary>
         private Action HotkeyHandler { get; set; }
 
         /// <summary>
-        /// String representation of hotkey used
+        /// Gets string representation of hotkey used
         /// </summary>
         private string HotkeyText
         {
@@ -121,34 +107,23 @@ namespace Tagger.ViewModels
                 {
                     hotkey += "Ctrl+";
                 }
+
                 if (this.ModifierKeys.HasFlag(ModifierKeys.Shift))
                 {
                     hotkey += "Shift+";
                 }
+
                 if (this.ModifierKeys.HasFlag(ModifierKeys.Alt))
                 {
                     hotkey += "Alt+";
                 }
+
                 if (this.ModifierKeys.HasFlag(ModifierKeys.Windows))
                 {
                     hotkey += "Win+";
                 }
 
-                return hotkey + this.Key.ToString();
-            }
-        }
-
-        /// <summary>
-        /// Cleanup resources
-        /// </summary>
-        protected override void OnDisposeManaged()
-        {
-            base.OnDisposeManaged();
-
-            if (this.GlobalHotkey != null)
-            {
-                this.GlobalHotkey.Dispose();
-                this.GlobalHotkey = null;
+                return hotkey + this.Key;
             }
         }
 
@@ -194,8 +169,40 @@ namespace Tagger.ViewModels
         }
 
         /// <summary>
+        /// Cleanup resources
+        /// </summary>
+        protected override void OnDisposeManaged()
+        {
+            base.OnDisposeManaged();
+
+            if (this.GlobalHotkey != null)
+            {
+                this.GlobalHotkey.Dispose();
+                this.GlobalHotkey = null;
+            }
+        }
+
+        /// <summary>
+        /// Check if key is a modifier (Ctrl, Alt, Shift)
+        /// </summary>
+        /// <param name="key">Key to check</param>
+        /// <returns>true if key is a modifier</returns>
+        private static bool IsModifierKey(Key key)
+        {
+            return key == Key.LeftShift
+                || key == Key.RightShift
+                || key == Key.LeftCtrl
+                || key == Key.RightCtrl
+                || key == Key.LeftAlt
+                || key == Key.RightAlt
+                || key == Key.LWin
+                || key == Key.RWin;
+        }
+
+        /// <summary>
         /// UnregisterHotkey command handler
         /// </summary>
+        /// <param name="parameter">The parameter is not used.</param>
         private void UnregisterHotkey(object parameter)
         {
             Check.Require(this.CanUnregisterHotkey(parameter), "You are not able to unregister hotkey");
@@ -211,6 +218,7 @@ namespace Tagger.ViewModels
         /// Test that verifies if UnregisterHotkey command can be invoked
         /// </summary>
         /// <returns>true if command could be invoked</returns>
+        /// <param name="parameter">The parameter is not used.</param>
         private bool CanUnregisterHotkey(object parameter)
         {
             return this.GlobalHotkey != null;
@@ -230,7 +238,10 @@ namespace Tagger.ViewModels
             var key = (ea.Key == Key.System) ? ea.SystemKey : ea.Key;
 
             // Ignore modifier keys
-            if (HotkeyViewModel.IsModifierKey(key)) { return; }
+            if (IsModifierKey(key))
+            {
+                return;
+            }
 
             // Set view model properties
             this.ModifierKeys = Keyboard.Modifiers;
@@ -242,4 +253,3 @@ namespace Tagger.ViewModels
         }
     }
 }
-
